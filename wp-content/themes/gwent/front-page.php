0e37eb2458e2f560b34eb9984f06b598b654951f
Jesus Loranca <jesus@substrakt.com>
2018-03-09 13:12:39 +0000

    Empezando con la cabecera

--- wp-content/themes/gwent/front-page.php
+++ wp-content/themes/gwent/front-page.php
@@ -1,12 +1,1 @@
 <?php
-require_once('app/clases/Jugador.php');
-
-$objeto = new Jugador($post);
-
-//var_dump($objeto->jugadores());
-
-foreach ($objeto->jugadores() as $jugador) {
-
-       echo $jugador['twitter'];
-	   
- }
