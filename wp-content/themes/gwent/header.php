6f50adf80c36745b63943aff338ca969edd7718e
Jesus Loranca <jesus@substrakt.com>
2018-03-12 12:42:55 +0000

    Cambios en el sistema de url, css y un poco de single.jugador

--- wp-content/themes/gwent/header.php
+++ wp-content/themes/gwent/header.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="wp-content\themes\gwent/style.css">
+    <link rel="stylesheet" href="<?php echo home_url() . '/wp-content/themes/gwent/style.css' ?>">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
@@ -14,7 +14,7 @@
 
 <body>
 
-<div style="background-image: url('wp-content/themes/gwent/imagenes/playgwent.com_en_1503999839_59a537601b90a9.47593042.jpg');">
+<div style="background-image: url('<?php echo home_url() . "/wp-content/themes/gwent/imagenes/playgwent.com_en_1503999839_59a537601b90a9.47593042.jpg" ?>');">
      <ul class="nav">
         <li class="nav-item">
             <a class="nav-link" href="#">Link</a>
@@ -30,3 +30,5 @@
         </li>
     </ul> 
 </div>
+
+<main>
--- wp-content/themes/gwent/single-jugador.php
+++ wp-content/themes/gwent/single-jugador.php
@@ -6,62 +6,72 @@ require_once('app/clases/Faccion.php');
 require_once('app/clases/NullObject.php');
 
 $jugador = new Jugador($post);
-
 ?>
 
 <div class="row imagenpequena col-12 col-md-12">
-<div class="col-12 col-md-4">
-<img src="<?php echo  $jugador->imagenJugador();?>">
+    
+    <div class="col-12 col-md-8">
+        <div>
+            <h1><?php echo  $jugador->nombreJugador();?></h1>
+        </div>
+
+        <div>
+            <h2><?php echo  $jugador->Equipo();?></h2>
+        </div>
+
+        <div>
+            <?php  if ($youtube = $jugador->youtube()) { echo '<a title="Youtube" href="' . $youtube . '"><i class="fab fa-youtube"></i></a>'; } ?>
+            <?php  if ($twitch = $jugador->twitch()) { echo '<a title="Twitch" href="' . $twitch . '"><i class="fab fa-twitch"></i></a>'; } ?>
+            <?php  if ($twitter = $jugador->twitter()) { echo '<a title="Twitter" href="' . $twitter . '"><i class="fab fa-twitter"></i></a>'; } ?>
+            <?php  if ($instagram = $jugador->instagram()) { echo '<a title="Instagram" href="' . $instagram . '"><i class="fab fa-instagram"></i></a>'; } ?>
+            <?php  if ($facebook = $jugador->facebook()) { echo '<a title="Facebook" href="' . $facebook . '"><i class="fab fa-facebook-square"></i></a>'; } ?>
+        </div>
+    </div>
+
+    <div class="col-12 col-md-4">
+        <img src="<?php echo  $jugador->imagenJugador();?>">
+    </div>
+
+    <div class="col-12 col-md-4">
+        <div>
+            <h2>Carta Favorita</h2>
+        </div>
+
+        <?php if ($jugador->cartaFavorita()->imagenCarta()): ?>
+            <div>
+                <?php echo  '<img src="'. $jugador->cartaFavorita()->imagenCarta() .'" alt="Twitter" />';?>
+            </div>
+        <?php endif; ?>
+    </div>
+
+    <div class="col-12 col-md-4">
+        <div>
+            <h2>Facción Favorita</h2>
+        </div>
+        <?php if ($jugador->faccionFavorita()->imagenFaccion()): ?>
+            <div>
+                <?php echo  '<img src="'. $jugador->faccionFavorita()->imagenFaccion() .'" alt="Twitter" />';?>
+            </div>
+        <?php endif; ?>
+
+        <div>
+            <h2><?php echo  $jugador->faccionFavorita()->nombreFaccion();?></h2>
+        </div>
+    </div>
+
+    <div class="col-12 col-md-4">
+        <div>
+            <h2>Lider Favorito</h2>
+        </div>
+
+        <div>
+            <?php echo  '<img src="'. $jugador->liderFavorito()->imagenCarta() .'" alt="Twitter" />';?>
+        </div>
+
+        <div>
+            <h2><?php echo  $jugador->liderFavorito()->nombreCarta();?></h2>
+        </div>
+    </div>
 </div>
-<div class="col-12 col-md-8">
-<div>
-<h1><?php echo  $jugador->nombreJugador();?></h1>
-</div>
-<div>
-<h2><?php echo  $jugador->Equipo();?></h2>
-</div>
-<div>
-<?php  if ($youtube=$jugador->youtube()){ echo '<a title="Youtube" href="'.$youtube.'"><i class="fab fa-youtube"></i></a>';}?>
-<?php  if ($twitch=$jugador->twitch()){ echo '<a title="Twitch" href="'.$twitch.'"><i class="fab fa-twitch"></i></a>';}?>
-<?php  if ($twitter=$jugador->twitter()){ echo '<a title="Twitter" href="'.$twitter.'"><i class="fab fa-twitter"></i></a>';}?>
-<?php  if ($instagram=$jugador->instagram()){ echo '<a title="Instagram" href="'.$instagram.'"><i class="fab fa-instagram"></i></a>';}?>
-<?php  if ($facebook=$jugador->facebook()){ echo '<a title="Facebook" href="'.$facebook.'"><i class="fab fa-facebook-square"></i></a>';}?>
-</div>
-</div>
-<div class="col-12 col-md-4">
-<div>
-<h2>Carta Favorita</h2>
-</div>
-<div>
-<?php echo  '<img src="'. $jugador->cartaFavorita()->imagenCarta() .'" alt="Twitter" />';?>
-</div>
-<div>
-<h2><?php echo  $jugador->cartaFavorita()->nombreCarta();?></h2>
-</div>
-</div>
-<div class="col-12 col-md-4">
-<div>
-<h2>Facción Favorita</h2>
-</div>
-<div>
-<?php echo  '<img src="'. $jugador->faccionFavorita()->imagenFaccion() .'" alt="Twitter" />';?>
-</div>
-<div>
-<h2><?php echo  $jugador->faccionFavorita()->nombreFaccion();?></h2>
-</div>
-</div>
-<div class="col-12 col-md-4">
-<div>
-<h2>Lider Favorito</h2>
-</div>
-<div>
-<?php echo  '<img src="'. $jugador->liderFavorito()->imagenCarta() .'" alt="Twitter" />';?>
-</div>
-<div>
-<h2><?php echo  $jugador->liderFavorito()->nombreCarta();?></h2>
-</div>
-</div>
-</div>
-<?php
-include('footer.php');
-?>
+
+<?php include('footer.php');?>
